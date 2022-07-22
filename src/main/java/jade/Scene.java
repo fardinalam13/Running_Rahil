51aaf08c7d83ce4ded4f08e27503facd832b3b73
Fardin Alam <alamfardin1521@gmail.com>
2022-07-22 21:22:39 +0600

    11th commit

--- src/main/java/jade/Scene.java
+++ src/main/java/jade/Scene.java
@@ -1,9 +1,11 @@
 package jade;
 
+import renderer.Renderer;
 import java.util.ArrayList;
 import java.util.List;
 public abstract class Scene {
 
+    protected Renderer renderer = new Renderer();
     protected Camera camera;
     private boolean isRunning = false;
     protected List<GameObject> gameObjects = new ArrayList<>();
@@ -19,6 +21,7 @@ public abstract class Scene {
     public void start() {
         for (GameObject go : gameObjects) {
             go.start();
+            this.renderer.add(go);
         }
         isRunning = true;
     }
@@ -29,8 +32,13 @@ public abstract class Scene {
         } else {
             gameObjects.add(go);
             go.start();
+            this.renderer.add(go);
         }
     }
 
     public abstract void update(float dt);
+
+    public Camera camera() {
+        return this.camera;
+    }
 }
