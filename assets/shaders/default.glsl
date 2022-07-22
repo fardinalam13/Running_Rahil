2d67c62ba92c9c762e11f0e3bf373644a6026e7a
Fardin Alam <alamfardin1521@gmail.com>
2022-07-22 17:00:43 +0600

    7th commit

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -3,16 +3,21 @@
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec4 aColor;
 
+uniform mat4 uProjection;
+uniform mat4 uView;
+
 out vec4 fColor;
 
 void main()
 {
     fColor = aColor;
-    gl_Position = vec4(aPos, 1.0);
+     gl_Position = uProjection * uView * vec4(aPos, 1.0);
 }
 
 #type fragment
 #version 330 core
+
+uniform float uTime;
 
 in vec4 fColor;
 
@@ -20,5 +25,6 @@ out vec4 color;
 
 void main()
 {
-    color = fColor;
+    float noise = fract(sin(dot(fColor.xy ,vec2(12.9898,78.233))) * 43758.5453);
+       color = fColor * noise;
 }
