a492c73db9de6d11934c2915bcff635e40277c9c
Fardin Alam <alamfardin1521@gmail.com>
2022-09-25 21:15:22 +0600

    24th commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Super Mario Java 2D Game
+
+This is a full screen 2D Java game like super mario with some basic creatures and maps.
+
+This game is implemented using the Java programming language, using the java graphics librieries.
+
+The used IDE is netbeans (I uploaded all project artifacts), also the game JAR file is uploaded.
+
+This game is free to use and the code is available to anyone to learn from it without any restrictions.
+
+Please visit my website. http://www.mohamedtalaat.net/
---
+++ src/game/GameEngine.java
@@ -0,0 +1,389 @@
+package game;
+
+import sprites.Creature;
+import sprites.Player;
+import sprites.PowerUp;
+import input.GameAction;
+import input.InputManager;
+import graphics.Sprite;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.Iterator;
+import javax.swing.JFrame;
+
+import test.GameCore;
+
+/**
+ * GameManager manages all parts of the game.
+ */
+public class GameEngine extends GameCore 
+{
+    
+    public static void main(String[] args) 
+    {
+                JFrame frame = new JFrame("Running Rahil");
+                //frame.setSize(700, 500); 
+                frame.setBounds(0, 0, 1240, 720);
+		frame.setBackground(Color.black);
+//                PopupMenu starter = null;
+//                frame.add(starter);
+                frame.setUndecorated(false);
+                frame.setVisible(true);
+                frame.setResizable(true);
+             //   frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+        new GameEngine().run();
+    }
+    
+    public static final float GRAVITY = 0.002f;
+    
+    public Point pointCache = new Point();
+    public TileMap map;
+    public MapLoader mapLoader;
+    public InputManager inputManager;
+    public TileMapDrawer drawer;
+    
+    public GameAction moveLeft;
+    public GameAction moveRight;
+    public GameAction jump;
+    public GameAction exit;
+    public int collectedStars=0;
+    public int numLives=3;
+   
+    public void init()
+    {
+        super.init();
+        
+        // set up input manager
+        initInput();
+        
+        // start resource manager
+        mapLoader = new MapLoader(screen.getFullScreenWindow().getGraphicsConfiguration());
+        
+        // load resources
+        drawer = new TileMapDrawer();
+        drawer.setBackground(mapLoader.loadImage("background3.jpg"));
+       
+        // load first map
+        map = mapLoader.loadNextMap();
+    }
+    
+    
+    /**
+     * Closes any resurces used by the GameManager.
+     */
+    public void stop() {
+        super.stop();
+        
+    }
+    
+    
+    public void initInput() {
+        moveLeft = new GameAction("moveLeft");
+        moveRight = new GameAction("moveRight");
+        jump = new GameAction("jump", GameAction.DETECT_INITAL_PRESS_ONLY);
+        exit = new GameAction("exit",GameAction.DETECT_INITAL_PRESS_ONLY);
+        
+        inputManager = new InputManager(screen.getFullScreenWindow());
+        inputManager.setCursor(InputManager.INVISIBLE_CURSOR);
+        
+        inputManager.mapToKey(moveLeft, KeyEvent.VK_LEFT);
+        inputManager.mapToKey(moveRight, KeyEvent.VK_RIGHT);
+        inputManager.mapToKey(jump, KeyEvent.VK_SPACE);
+        inputManager.mapToKey(exit, KeyEvent.VK_ESCAPE);
+    }
+    
+    
+    public void checkInput(long elapsedTime) 
+    {
+        
+        if (exit.isPressed()) {
+            stop();
+        }
+        
+        Player player = (Player)map.getPlayer();
+        if (player.isAlive()) 
+        {
+            float velocityX = 0;
+            if (moveLeft.isPressed()) 
+            {
+                velocityX-=player.getMaxSpeed();
+            }
+            if (moveRight.isPressed()) {
+                velocityX+=player.getMaxSpeed();
+            }
+            if (jump.isPressed()) {
+                player.jump(false);//true
+            }
+            player.setVelocityX(velocityX);
+        }
+        
+    }
+    
+    
+    public void draw(Graphics2D g) {
+        
+        drawer.draw(g, map, screen.getWidth(), screen.getHeight());
+        g.setColor(Color.BLACK);
+        g.drawString("Press ESC for EXIT.",10.0f,20.0f);
+        g.setColor(Color.GREEN);
+        g.drawString("Coins: "+collectedStars,300.0f,20.0f);
+        g.setColor(Color.YELLOW);
+        g.drawString("Lives: "+(numLives),500.0f,20.0f );
+        g.setColor(Color.WHITE);
+        g.drawString("Level: "+mapLoader.currentMap,700.0f,20.0f); 
+        
+    }
+  
+    /**
+     * Gets the current map.
+     */
+    public TileMap getMap() {
+        return map;
+    }
+    /**
+     * Gets the tile that a Sprites collides with. Only the
+     * Sprite's X or Y should be changed, not both. Returns null
+     * if no collision is detected.
+     */
+    public Point getTileCollision(Sprite sprite, float newX, float newY) 
+    {
+        float fromX = Math.min(sprite.getX(), newX);
+        float fromY = Math.min(sprite.getY(), newY);
+        float toX = Math.max(sprite.getX(), newX);
+        float toY = Math.max(sprite.getY(), newY);
+
+        // get the tile locations
+        int fromTileX = TileMapDrawer.pixelsToTiles(fromX);
+        int fromTileY = TileMapDrawer.pixelsToTiles(fromY);
+        int toTileX = TileMapDrawer.pixelsToTiles(toX + sprite.getWidth() - 1);
+        int toTileY = TileMapDrawer.pixelsToTiles(toY + sprite.getHeight() - 1);
+        
+        // check each tile for a collision
+        for (int x=fromTileX; x<=toTileX; x++) {
+            for (int y=fromTileY; y<=toTileY; y++) {
+                if (x < 0 || x >= map.getWidth() || map.getTile(x, y) != null) {
+                    // collision found, return the tile
+                    pointCache.setLocation(x, y);
+                    return pointCache;
+                }
+            }
+        }
+        
+        // no collision found
+        return null;
+    }
+    
+    
+    /**
+     * Checks if two Sprites collide with one another. Returns
+     * false if the two Sprites are the same. Returns false if
+     * one of the Sprites is a Creature that is not alive.
+     */
+    public boolean isCollision(Sprite s1, Sprite s2) {
+        // if the Sprites are the same, return false
+        if (s1 == s2) {
+            return false;
+        }
+        
+        // if one of the Sprites is a dead Creature, return false
+        if (s1 instanceof Creature && !((Creature)s1).isAlive()) {
+            return false;
+        }
+        if (s2 instanceof Creature && !((Creature)s2).isAlive()) {
+            return false;
+        }
+        
+        // get the pixel location of the Sprites
+        int s1x = Math.round(s1.getX());
+        int s1y = Math.round(s1.getY());
+        int s2x = Math.round(s2.getX());
+        int s2y = Math.round(s2.getY());
+        
+        // check if the two sprites' boundaries intersect
+        return (s1x < s2x + s2.getWidth() && s2x < s1x + s1.getWidth() && s1y < s2y + s2.getHeight() && s2y < s1y + s1.getHeight());
+    }
+
+    /**
+     * Gets the Sprite that collides with the specified Sprite,
+     * or null if no Sprite collides with the specified Sprite.
+     */
+    public Sprite getSpriteCollision(Sprite sprite) {
+        // run through the list of Sprites
+        Iterator i = map.getSprites();
+        while (i.hasNext()) {
+            Sprite otherSprite = (Sprite)i.next();
+            if (isCollision(sprite, otherSprite)) {
+                // collision found, return the Sprite
+                return otherSprite;
+            }
+        }
+        
+        // no collision found
+        return null;
+    }
+    
+    
+    /**
+     * Updates Animation, position, and velocity of all Sprites
+     * in the current map.
+     */
+    public void update(long elapsedTime) {
+        Creature player = (Creature)map.getPlayer();
+        
+        
+        // player is dead! start map over
+        if (player.getState() == Creature.STATE_DEAD) {
+            map = mapLoader.reloadMap();
+            return;
+        }
+        
+        // get keyboard/mouse input
+        checkInput(elapsedTime);
+        
+        // update player
+        updateCreature(player, elapsedTime);
+        player.update(elapsedTime);
+        
+        // update other sprites
+        Iterator i = map.getSprites();
+        while (i.hasNext()) {
+            Sprite sprite = (Sprite)i.next();
+            if (sprite instanceof Creature) {
+                Creature creature = (Creature)sprite;
+                if (creature.getState() == Creature.STATE_DEAD) {
+                    i.remove();
+                } else {
+                    updateCreature(creature, elapsedTime);
+                }
+            }
+            // normal update
+            sprite.update(elapsedTime);
+        }
+    }
+    
+    
+    /**
+     * Updates the creature, applying gravity for creatures that
+     * aren't flying, and checks collisions.
+     */
+    private void updateCreature(Creature creature,
+            long elapsedTime) {
+        
+        // apply gravity
+        if (!creature.isFlying()) {
+            creature.setVelocityY(creature.getVelocityY() +
+                    GRAVITY * elapsedTime);
+        }
+        
+        // change x
+        float dx = creature.getVelocityX();
+        float oldX = creature.getX();
+        float newX = oldX + dx * elapsedTime;
+        Point tile =
+                getTileCollision(creature, newX, creature.getY());
+        if (tile == null) {
+            creature.setX(newX);
+        } else {
+            // line up with the tile boundary
+            if (dx > 0) {
+                creature.setX(
+                        TileMapDrawer.tilesToPixels(tile.x) -
+                        creature.getWidth());
+            } else if (dx < 0) {
+                creature.setX(
+                        TileMapDrawer.tilesToPixels(tile.x + 1));
+            }
+            creature.collideHorizontal();
+        }
+        if (creature instanceof Player) {
+            checkPlayerCollision((Player)creature, false);
+        }
+        
+        // change y
+        float dy = creature.getVelocityY();
+        float oldY = creature.getY();
+        float newY = oldY + dy * elapsedTime;
+        tile = getTileCollision(creature, creature.getX(), newY);
+        if (tile == null) {
+            creature.setY(newY);
+        } else {
+            // line up with the tile boundary
+            if (dy > 0) {
+                creature.setY(
+                        TileMapDrawer.tilesToPixels(tile.y) - creature.getHeight());
+            } else if (dy < 0) {
+                creature.setY(TileMapDrawer.tilesToPixels(tile.y + 1));
+            }
+            creature.collideVertical();
+        }
+        if (creature instanceof Player) {
+            boolean canKill = (oldY < creature.getY());
+            checkPlayerCollision((Player)creature, canKill);
+        }
+        
+    }
+
+    /**
+     * Checks for Player collision with other Sprites. If
+     * canKill is true, collisions with Creatures will kill
+     * them.
+     */
+    public void checkPlayerCollision(Player player,boolean canKill) {
+        if (!player.isAlive()) {
+            return;
+        }
+        // check for player collision with other sprites
+        Sprite collisionSprite = getSpriteCollision(player);
+        if (collisionSprite instanceof PowerUp) {
+            acquirePowerUp((PowerUp)collisionSprite);
+        } else if (collisionSprite instanceof Creature) {
+            Creature enemy = (Creature)collisionSprite;
+            if (canKill) {
+                // kill the badguy and make player bounce
+                enemy.setState(Creature.STATE_DYING);
+                player.setY(enemy.getY() - player.getHeight());
+                player.jump(true);
+            } else {
+                // player dies!
+                player.setState(Creature.STATE_DYING);
+                numLives--;
+                if(numLives==0) {
+                    stop();
+                }
+            }
+        }
+    }
+    
+    
+    /**
+     * Gives the player the speicifed power up and removes it
+     * from the map.
+     */
+    public void acquirePowerUp(PowerUp powerUp) {
+        // remove it from the map
+        map.removeSprite(powerUp);
+        
+        if (powerUp instanceof PowerUp.Star) {
+            // do something here, like give the player points
+            collectedStars++;
+            if(collectedStars==10) 
+            {
+                numLives++;
+                collectedStars=0;
+            }
+            
+        } else if (powerUp instanceof PowerUp.Music) {
+            // change the music
+            
+        } else if (powerUp instanceof PowerUp.Goal) {
+            // advance to next map      
+      
+            map = mapLoader.loadNextMap();
+            
+        }
+    }
+    
+      
+}
